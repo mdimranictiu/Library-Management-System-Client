68d014afcf10fea6bd0b1e5e7d95d248d1077399
mdimranictiu <imranictiu11@gmail.com>
2024-12-25 13:22:25 +0600

    slided update

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,19 +1,26 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState } from "react";
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// import image
+import slider1 from "../../assets/slider01.webp";
+import slider2 from "../../assets/slider02.webp";
+import slider03 from "../../assets/slider03.webp";
+import slider04 from "../../assets/slider04.png";
+import slider05 from "../../assets/slider05.jpg";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-import './style.css';
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "./style.css";
 
 // Import required modules
-import { Pagination, Navigation } from 'swiper/modules';
+import { Pagination, Navigation } from "swiper/modules";
 
 const Slider = () => {
   return (
-    <div className='py-10'>
+    <div className="py-10">
       <Swiper
         slidesPerView={1}
         spaceBetween={30}
@@ -26,23 +33,73 @@ const Slider = () => {
         className="mySwiper"
       >
         <SwiperSlide>
-          <div >
-            <img src="https://www.maynoothuniversity.ie/sites/default/files/assets/images/studentsforweb2.jpg" alt="Slide 1" />
+          <div className="w-full h-[500px]">
+            <div className="w-full h-full  mx-auto">
+              <img className="w-full  opacity-80" src={slider1} alt="Slide 1" />
+            </div>
             <div className="absolute inset-0 flex flex-col items-center justify-center">
-            <h2 className=" text-3xl  font-bold  px-4 py-2 rounded">
-            Welcome to Our Library Management System </h2>
-            <br />          
-          </div>
+              <h2 className=" max-sm:text-3xl  text-4xl  font-bold text-[#008575] opacity-90 bg-white/75  px-4 py-2 rounded">
+                Welcome to Our Library Management System{" "}
+              </h2>
+              <br />
+            </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div >
-            <img src="https://i.ibb.co.com/cgBrHgW/R.jpg" alt="Slide 2" />
-            <p>Slide 2 Text</p>
+          <div className="w-full h-[500px]">
+            <div className="w-full h-full mx-auto">
+              <img className="w-full opacity-80 " src={slider2} alt="Slide 2" />
+            </div>
+            <div className="absolute inset-0 flex flex-col items-center justify-center">
+              <h2 className=" text-4xl max-sm:text-3xl  font-bold text-[#008575] opacity-90 bg-white/75  px-4 py-2 rounded">
+                Explore a Wide Range of Book Categories
+              </h2>
+              <br />
+              <button class="btn border-none text-white  w-[200px] text-[16px] hover:bg-white outline-none hover:text-[#008575] bg-[#008575]">
+                Explore Categories
+              </button>
+            </div>
           </div>
         </SwiperSlide>
-        
-        {/* Add more slides as needed */}
+        <SwiperSlide>
+          <div className="w-full h-[500px]">
+            <div className="w-full h-full mx-auto">
+              <img className="w-full opacity-80" src={slider03} alt="Slide 3" />
+            </div>
+            <div className="absolute inset-0 flex flex-col items-center justify-center">
+              <h2 className=" max-sm:text-3xl  text-4xl  font-bold text-[#008575] opacity-90 bg-white/75   px-4 py-2 rounded">
+                Borrow Books in Just a Few Clicks
+              </h2>
+              <br />
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="w-full h-[500px]">
+            <div className="w-full h-full mx-auto">
+              <img className="w-full opacity-80" src={slider04} alt="Slide 4" />
+            </div>
+            <div className="absolute inset-0 flex flex-col items-center justify-center">
+              <h2 className="  text-4xl max-sm:text-3xl  font-bold text-[#008575] opacity-90 bg-white/75  px-4 py-2 rounded">
+                Access Digital Books and Resources Anytime
+              </h2>
+              <br />
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="w-full h-[500px]">
+            <div className="w-full h-full mx-auto">
+              <img className="w-full opacity-80" src={slider05} alt="Slide 5" />
+            </div>
+            <div className="absolute inset-0 flex flex-col items-center justify-center">
+              <h2 className=" max-sm:text-3xl  text-4xl  font-bold text-[#008575] opacity-90 bg-white/75  px-4 py-2 rounded">
+                Manage Your Account and Borrowing History
+              </h2>
+              <br />
+            </div>
+          </div>
+        </SwiperSlide>
       </Swiper>
     </div>
   );
