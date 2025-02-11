9414939d934dbf4b0ad7dcca4d60365a3b5da550
mdimranictiu <imranictiu11@gmail.com>
2025-02-11 13:51:02 +0600

    responsive design for card

--- src/Firebase/Firebase.init.js
+++ src/Firebase/Firebase.init.js
@@ -7,6 +7,13 @@ const firebaseConfig = {
  
 // pasete your information here
 
+    apiKey: "AIzaSyAXQMzzN00cv6zUkpqdyAqF6579-fkyW74",
+    authDomain: "library-management-syste-4cae8.firebaseapp.com",
+    projectId: "library-management-syste-4cae8",
+    storageBucket: "library-management-syste-4cae8.firebasestorage.app",
+    messagingSenderId: "965155436609",
+    appId: "1:965155436609:web:0d45039a2812a59a5caec0"
+  
 };
 
 // Initialize Firebase
